5d250c211bb36afa857a99095ef2ee1f038a5219
Rene Fuerst <seiya@me.com>
2015-01-07 16:17:28 +0100

    first upload

---
+++ HelloWorldServlets/src/at/thinksecret/helloworld/servlets/HelloWorld.java
@@ -0,0 +1,44 @@
+package at.thinksecret.helloworld.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hiddenservlets/helloworld2.do")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		double sqrtvalue = Math.sqrt(4.0);
+		
+		out.println("<html><body><h1 align='center'>Hello World Servlet 2</h1><br>The Square root of 4.0 is: " + sqrtvalue + "</body></html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
